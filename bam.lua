1bdbd8904057f7cba7a3724b8ec3647442080b75
Bamcane <diandaokui@qq.com>
2023-12-16 21:26:34 +0800

    change file name

--- bam.lua
+++ bam.lua
@@ -268,7 +268,7 @@ function build(settings)
 	end
 
 	-- build server
-	server_exe = Link(server_settings, "teeworlds_srv", engine, server,
+	server_exe = Link(server_settings, "teewar_srv", engine, server,
 		game_shared, game_server, zlib, md5, server_link_other, json, teeuniverses)
 
 	serverlaunch = {}
